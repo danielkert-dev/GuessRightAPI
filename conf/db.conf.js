dcc508227856d4278a56911ac1d0b3fef6139632
Daniel Kert <danielkertdev@gmail.com>
2023-05-25 15:32:14 +0300

    commit may 25 15:32

--- conf/db.conf.js
+++ conf/db.conf.js
@@ -4,10 +4,13 @@ module.exports = createPool({
     multipleStatements: true,
     charset: 'UTF8_GENERAL_CI',
     user: 'client', // this needs to match the node.js process user
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    connectionLimit: 10,
 })
