a15ff27a7eb97dee1b3ff0ff1f9460e50ed8db5e
Daniel Kert <danielkertdev@gmail.com>
2023-05-26 15:21:18 +0300

    commit may 26 15:21

--- api/quiz/quiz.controller.js
+++ api/quiz/quiz.controller.js
@@ -154,9 +154,6 @@ module.exports = {
       if (!results || results.length === 0) {
         return error404(res, results);
       }
-      if (results.data.question_amount === 0) {
-        return error404(res, results);
-      }
       return response200(res, results);
     })
   },
